4ede7ab7ba8063dd011024b28cd5af2b5d31dfbd
Omkar Desai <omkar.desai916@gmail.com>
2022-01-08 21:37:08 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import Pagination from "./Pagination";
-import "./app.css";
+// import "./app.css";
 
 export default function () {
   const [data, setData] = useState([]);
